e4359f49fce808b92f9b391c39bf1f941f61f224
wanghongcai <524901165@qq.com>
2016-02-01 16:48:04 +0800

    测试用例类

---
+++ src/test_project/Test_demo.java
@@ -0,0 +1,10 @@
+package test_project;
+
+public class Test_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sdfosdlfjdsfdd");
+	}
+
+}
