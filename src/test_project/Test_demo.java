cc7d2ef0b4445063f9ae6551a7688c5482ff0d1d
wanghongcai <524901165@qq.com>
2016-02-01 17:14:50 +0800

    添加一条输出

--- src/test_project/Test_demo.java
+++ src/test_project/Test_demo.java
@@ -5,6 +5,7 @@ public class Test_demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("what are you doing?");
+		System.out.println("sdfdfsdffdsfsdfsdfds");
 	}
 
 }
